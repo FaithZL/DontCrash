e770964b09b5d75615760360a58a7c96762c4cf1
SATAN_Z <249002031@qq.com>
2016-01-19 13:36:05 +0800

    add base car

---
+++ Classes/Car/BaseCar.hpp
@@ -0,0 +1,26 @@
+//
+//  BaseCar.hpp
+//  DontCrash
+//
+//  Created by SATAN_Z on 16/1/19.
+//
+//
+
+#ifndef BaseCar_hpp
+#define BaseCar_hpp
+
+#include "cocos2d.h"
+#include "../utils/FSM/FSM.h"
+
+class FSM;
+
+class BaseCar : public cocos2d::Sprite , public FSM
+{
+    
+public:
+    
+    
+    
+};
+
+#endif /* BaseCar_hpp */
