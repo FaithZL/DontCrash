2a4e309183b1afe001ec709d69c7d7ba7c53249a
SATAN_Z <249002031@qq.com>
2016-01-23 15:01:34 +0800

    ...

---
+++ Classes/utils/Signal.cpp
@@ -0,0 +1,9 @@
+//
+//  Signal.cpp
+//  DontCrash
+//
+//  Created by SATAN_Z on 16/1/23.
+//
+//
+
+#include "Signal.hpp"
---
+++ Classes/utils/Signal.hpp
@@ -0,0 +1,14 @@
+//
+//  Signal.hpp
+//  DontCrash
+//
+//  Created by SATAN_Z on 16/1/23.
+//
+//
+
+#ifndef Signal_hpp
+#define Signal_hpp
+
+#include <stdio.h>
+
+#endif /* Signal_hpp */
